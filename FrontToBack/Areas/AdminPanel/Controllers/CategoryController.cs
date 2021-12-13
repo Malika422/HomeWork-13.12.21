ce790a6d04828c16c5cf034232dd2bac245e690c
Malika422 <malikays@code.edu.az>
2021-12-13 20:24:55 +0400

    HomeWorkCrudFileUpload

---
+++ FrontToBack/Areas/AdminPanel/Controllers/CategoryController.cs
@@ -0,0 +1,43 @@
+﻿using FrontToBack.DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBack.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+        public CategoryController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var categpries = await _dbContext.Categories.ToListAsync();
+            return View(categpries);
+        }
+       public async Task<IActionResult> Detail(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var category = await _dbContext.Categories.FindAsync(id);
+
+            if (category == null)
+
+                return NotFound();
+
+            return View(category);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
